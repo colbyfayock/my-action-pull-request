3cb48d8ebfc66161cac4956e87a87aea89994ff0
colbyfayock <fayock@gmail.com>
2021-01-30 22:24:02 -0500

    Step 2: adding a Thank You comment to new pull requests

--- src/action.js
+++ src/action.js
@@ -1,5 +1,16 @@
+const github = require('@actions/github');
+
 async function run() {
-  console.log('Hello, world!');
+  const octokit = github.getOctokit(GITHUB_TOKEN);
+
+  const { context = {} } = github;
+  const { pull_request } = context.payload;
+
+  await octokit.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.'
+  });
 }
 
 run();
